b92513703a8928a21e2a929c068285ebd2471ebc
Pruthvi_cloud_deveops <107435692+Pruthvi360@users.noreply.github.com>
2023-12-05 11:04:38 +0530

    Update open-ports-in-linux.sh.

--- dhcp/open-ports-in-linux.sh
+++ dhcp/open-ports-in-linux.sh
@@ -1,15 +1,19 @@
-#!/bin/bash
-# this script is designed to install the ISC Kea DHCP4 server to a Debian Linux system. It should also work on a Ubuntu system.
-# run this as root (for testing purposes). 
-# Otherwise, your account will need to be a sudoer and you will need to add `sudo` before `bash` on line 12, and at the beggining of lines 10, 11, and 13. 
-# make sure you chmod +x this script before running.
-# this script installs kea-1-8, for newer versions, modify line 12
- 
-start=$SECONDS
-printf "\n\033[7;31mExecuting the script.........\033[0m\n\n"
-apt update && apt upgrade -y
-apt install curl vim -y
-curl -1sLf 'https://dl.cloudsmith.io/public/isc/kea-2-0/cfg/setup/bash.deb.sh'| bash
-apt install -y isc-kea-common isc-kea-dhcp4-server
-printf "\n\033[7;32mCOMPLETE! \033[0m"
-printf "\nTime to complete = %s seconds.\n\n" "$SECONDS"
+#!/bin/bash 
+# Firewall Rules script for FreeIPA server.
+# This script opens the following TCP and UDP ports on a Fedora Server system.
+# Create a directory named scripts in /~  and save the script there.
+# You will have to modify permissions with chmod (as shown below) and run as root.
+# chmod +x rules.sh
+# If you are not installing the DNS portion of FreeIPA then port 53 can be removed from the 't' and 'u' lists. 
+
+# start script
+  for t in 80 443 389 636 88 464 53 
+  do 
+  firewall-cmd  --permanent --add-port=$[t]/tcp 
+  done 
+
+  for u in 88 464 53 123 
+  do 
+  firewall-cmd  --permanent --add-port=$[u]/udp 
+  done 
+# end script
